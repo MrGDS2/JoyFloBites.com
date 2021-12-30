7c126a2cf90e4a203523a90620c44599f2d06360
MrGDS2 <mrsonicthunder@gmail.com>
2021-12-30 13:28:09 -0500

    slack post axios

--- src/Components/SlackCommunityPage/SlackCommunityPage.js
+++ src/Components/SlackCommunityPage/SlackCommunityPage.js
@@ -1,43 +1,78 @@
 
 import React, { useState } from 'react';
-import { Form, FormGroup, Label, Input } from 'reactstrap';
 import Button from 'react-bootstrap/Button';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import FadeIn from 'react-fade-in';
 import flour_logo from '../../Images/joyflo-bites-v1.png';
 import swal from 'sweetalert2';
+import axios from 'axios';
 import ReCAPTCHA from "react-google-recaptcha";
 import './SlackCommunityPage.scss';
 
 
 
 const SlackCommunityPage = () => {
-
+    
+    const webHookURL="https://hooks.slack.com/services/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";
     const [isVerified, setVerification] = useState(false);
-
-
-    const [fullName, setFullName] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState('');
-    const [email, setEmail] = useState('');
-    const [service, setService] = useState('');
     const [message, setMessage] = useState('');
 
+  
+    // let res =  axios.post(webHookURL, JSON.stringify(data), {
+    //     withCredentials: false,
+    //     transformRequest: [(data, headers) => {
+    //         delete headers.post["Content Type"]
 
-    const isEnabled = email.length > 0 && fullName.length > 0 &&
-        phoneNumber.length > 0 && service.length > 0 && message.length > 0 && isVerified;
+    //         return data;
+    //     }]
+
+    // })
+
+    const data ={
+        "text": `${message}`
+    }
+
+    
+    axios.post(webHookURL,JSON.stringify(data))
+    .then(res => {
+      console.log(res);
+      console.log(res.data);
+    })
+
+
+    // if (res === 200) {
+    //     swal.fire("SENT!");
+    //     setMessage("");
+    // } else {
+    //     swal.fire("Message Failed!");
+
+    // }
+
+    if(res===200){
+        swal.fire("SENT!");
+        setMessage("");
+    }
+    else{
+        swal.fire("Message Failed!");
+
+    }
+
+  
 
     const verifyCallback = () => {
         setVerification(true);
     }
 
 
-    const handleSubmit = () => {
-
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        swal.fire("SENT!");
 
 
     }
 
+   // console.log("feedback " + message);
 
     return (
 
@@ -48,7 +83,7 @@ const SlackCommunityPage = () => {
                     
                     <div className="row row-centered">
                         <div className="col-md-12">
-                            <img src={flour_logo} width="600" height="500" />
+                            <img src={flour_logo} width="600" height="500" alt="logo" />
                             <h1 className="feed-text mt-5">Your FeedBack Is Important!</h1>
                         </div>
 
@@ -73,9 +108,14 @@ const SlackCommunityPage = () => {
                                     rows={4}
                                     placeholder="Enter Text Here"
                                     variant="filled"
+                                    value={message}
+                                    onChange={(e)=> setMessage(e.target.value)}
+
                                 />
                             </Box>
-                            <Button variant="dark">Submit</Button>
+                            <Button variant="dark"
+                             onClick={(e)=>handleSubmit(e)}
+                            >Submit</Button>
                         </div>
                     </div>
                 </div>
