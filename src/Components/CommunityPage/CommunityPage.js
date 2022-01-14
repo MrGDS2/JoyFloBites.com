819286df5a899e2340b15ad3c32e798ead9f113f
MrGDS2 <mrsonicthunder@gmail.com>
2022-01-14 08:43:13 -0500

    removed photos

--- src/Components/CommunityPage/CommunityPage.js
+++ src/Components/CommunityPage/CommunityPage.js
@@ -1,7 +1,4 @@
 import AwesomeSlider from 'react-awesome-slider';
-import image1 from '../../Images/cookie_flo_review2.png';
-import image2 from '../../Images/cookie_flo_review_daddy.jpg';
-import image3 from '../../Images/cookie_flo_review3.png';
 import FadeIn from 'react-fade-in';
 import 'react-awesome-slider/dist/styles.css';
 
@@ -12,9 +9,9 @@ const CommunityPage = () => {
   const slider = (
     <FadeIn transitionDuration="4000">
       <AwesomeSlider className="mb-1" >
-        <div data-src={image1} />
-        <div data-src={image2} />
-        <div data-src={image3} />
+        <div data-src={'https://firebasestorage.googleapis.com/v0/b/joyflobites.appspot.com/o/Reviews%2Fcookie_flo_review2.png?alt=media&token=3c2adff5-7118-459b-b857-29c0172483af'} />
+        <div data-src={'https://firebasestorage.googleapis.com/v0/b/joyflobites.appspot.com/o/Reviews%2Fcookie_flo_review_daddy.jpg?alt=media&token=af8b8800-12c4-47ff-a985-d0b882a4556d'} />
+        <div data-src={'https://firebasestorage.googleapis.com/v0/b/joyflobites.appspot.com/o/Reviews%2Fcookie_flo_review3.png?alt=media&token=5dc08f82-d2b5-492e-9fc1-220a30ca5e89'} />
       </AwesomeSlider>
     </FadeIn>
 
